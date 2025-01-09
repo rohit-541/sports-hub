975ec132fe2da12af21348e5639967089db94b8f
rohit-541 <rohitkumar240iit@gmail.com>
2025-01-10 03:15:10 +0530

    Removed MemberShip code

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
-import { MembershipModule } from './membership/membership.module';
 import { ConfigModule } from '@nestjs/config';
 import { EventsModule } from './events/events.module';
 
 
 @Module({
-  imports: [ConfigModule.forRoot(), UserModule,MembershipModule, EventsModule],
+  imports: [ConfigModule.forRoot(), UserModule, EventsModule],
   controllers: [AppController],
   providers: [AppService],
 })
